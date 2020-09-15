de7171a30220a30e7562b654ad2b8be04d2736e3
Ayush <ayush@gocomet.in>
2020-09-16 05:24:45 +0530

    version changed

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 docker build -t ayushlodhi/jenkins -f Dockerfile.master .
 docker push ayushlodhi/jenkins
-docker tag ayushlodhi/jenkins ayushlodhi/jenkins:v1.3
-docker push ayushlodhi/jenkins:v1.3
+docker tag ayushlodhi/jenkins ayushlodhi/jenkins:v1.5
+docker push ayushlodhi/jenkins:v1.5
 
 docker build -t ayushlodhi/jnlp-slave -f Dockerfile.slave .
 docker push ayushlodhi/jnlp-slave
-docker tag ayushlodhi/jnlp-slave ayushlodhi/jnlp-slave:v1.3
-docker push ayushlodhi/jnlp-slave:v1.3
+docker tag ayushlodhi/jnlp-slave ayushlodhi/jnlp-slave:v1.5.1
+docker push ayushlodhi/jnlp-slave:v1.5.1
